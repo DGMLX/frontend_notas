6285822e0f4d7e4205b1fe483bb88294a8a3a6e5
Diego <diegoaltdev@gmail.com>
2025-04-24 23:46:29 -0400

    config variables de entorno

--- src/axios/consultasAxios.js
+++ src/axios/consultasAxios.js
@@ -1,12 +1,16 @@
 import axios from "axios"
 
-export const obtenerNotasRequest = async () => await axios.get("https://backendnotas-production-1944.up.railway.app/notas");
-export const obtenerNotasEliminadasRequest = async () => await axios.get("https://backendnotas-production-1944.up.railway.app/notasEliminadas");
-export const obtenerCategoriasRequest = async () => await axios.get("https://backendnotas-production-1944.up.railway.app/categorias");
-export const obtenerIdCategoriaRequest = async (nombre) => await axios.get(`https://backendnotas-production-1944.up.railway.app/obtenerIdCategoria/${nombre}`);
-export const obtenerNotasFiltradasRequest = async(categoria) => await axios.get(`https://backendnotas-production-1944.up.railway.app/obtenerNotasFiltradas/${categoria}`)
-export const agregarNotaRequest = async(data) => await axios.post("https://backendnotas-production-1944.up.railway.app/agregarNota",data)
-export const eliminarNotaRequest = async (id) => await axios.put(`https://backendnotas-production-1944.up.railway.app/eliminarNota/${id}`)
-export const restaurarNotaRequest = async (id) => await axios.put(`https://backendnotas-production-1944.up.railway.app/restaurarNota/${id}`)
-export const actualizarNotaRequest = async(data) => await axios.put("https://backendnotas-production-1944.up.railway.app/actualizarNota",data)
-export const eliminarNotaDefinitivoRequest = async (id) => await axios.delete(`https://backendnotas-production-1944.up.railway.app/eliminarNotaDefinitiva/${id}`)
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL_PROD,
+});
+
+export const obtenerNotasRequest = async () => await api.get("/notas");
+export const obtenerNotasEliminadasRequest = async () => await api.get("/notasEliminadas");
+export const obtenerCategoriasRequest = async () => await api.get("/categorias");
+export const obtenerIdCategoriaRequest = async (nombre) => await api.get(`/obtenerIdCategoria/${nombre}`);
+export const obtenerNotasFiltradasRequest = async (categoria) => await api.get(`/obtenerNotasFiltradas/${categoria}`);
+export const agregarNotaRequest = async (data) => await api.post("/agregarNota", data);
+export const eliminarNotaRequest = async (id) => await api.put(`/eliminarNota/${id}`);
+export const restaurarNotaRequest = async (id) => await api.put(`/restaurarNota/${id}`);
+export const actualizarNotaRequest = async (data) => await api.put("/actualizarNota", data);
+export const eliminarNotaDefinitivoRequest = async (id) => await api.delete(`/eliminarNotaDefinitiva/${id}`);
